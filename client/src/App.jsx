909f697a1d18212ee92af0c3c8a5c6a56cf463b3
aravindgopan <aravindgopan.dev@gmail.com>
2024-08-21 16:49:40 +0000

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from "../src/pages/Home"
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashbord from './pages/Dashbord'
+import Projects from './pages/Projects'
 function App() {
   return (
-    <div className='bg-pink-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/sign-in' element={<Signin></Signin>}></Route>
+        <Route path='/sign-up' element={<Signup></Signup>}></Route>
+        <Route path='/dashboard' element={<Dashbord></Dashbord>}></Route>
+        <Route path='/projects' element={<Projects></Projects>}></Route>
+
+
+
+      </Routes>
+    
+    
+    </BrowserRouter>
   )
 }
 
